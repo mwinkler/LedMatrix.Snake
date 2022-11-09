7575fd166cfd7fed3e43c9e8c86b20eae9a71fd3
Matthias Winkler <mwinkler@gmx.ch>
2022-11-09 19:54:18 +0100

    Create simple snke

--- code.py
+++ code.py
@@ -1,35 +1,105 @@
 import board
 import displayio
-import adafruit_imageload
 import time
 from adafruit_matrixportal.matrix import Matrix
 import random
+from analogio import AnalogIn
+
+def create_food():
+    return (random.randint(0, FIELD_WIDTH), random.randint(0, FIELD_HEIGHT))
+def get_random_color():
+    return random.randint(1, 4)
 
 # init display
 FIELD_WIDTH = 32
 FIELD_HEIGHT = 32
 matrix = Matrix(width=FIELD_WIDTH, height=FIELD_HEIGHT)
 
-# load image
-#displayio.OnDiskBitmap()
-sprite_sheet, palette = adafruit_imageload.load("tiles.bmp", bitmap=displayio.Bitmap, palette=displayio.Palette)
-palette.make_transparent(0)
+# colors
+palette = displayio.Palette(5)
+palette[0] = 0x000000   # black
+palette[1] = 0x00FF00   # green
+palette[2] = 0xFF0000   # red
+palette[3] = 0x0000FF   # blue
+palette[4] = 0xFFFFDD   # yellow
 
-# create group and tile group
-tiles = list(map(lambda i: displayio.TileGrid(sprite_sheet, pixel_shader=palette, tile_width=4, default_tile=i), range(5)))
+# play field
+grid = displayio.Bitmap(FIELD_WIDTH, FIELD_HEIGHT, len(palette))
+field_tile = displayio.TileGrid(grid, pixel_shader=palette)
 
+# snake
+snake_pos = (int(FIELD_WIDTH / 2), int(FIELD_HEIGHT / 2)) # (x, y)
+snake_direction = (1, 0)    # (x, y)
+snake_color = get_random_color()
+snake_body = [snake_pos]
+snake_length = 5
+snake_speed = 20 
+food_pos = create_food()
+food_color = get_random_color()
+
+# init display
 group = displayio.Group(scale=1)
-
-for tile in tiles:
-    group.append(tile)
-    
-# show
+group.append(field_tile)
 matrix.display.show(group)
 
-#tile.transpose_xy = True
+# init joystick
+joy_y = AnalogIn(board.A1) # 0=up   65k=down
+joy_x = AnalogIn(board.A2) # 0=rigt 65k=left
+
+frame_counter = 0
 
 while True:
-    for tile in tiles:
-        tile.x = min(FIELD_HEIGHT - 4, max(0, tile.x + random.randrange(-1, 2)))
-        tile.y = min(FIELD_HEIGHT - 4, max(0, tile.y + random.randrange(-1, 2)))
-    time.sleep(0.05)
+    
+    frame_counter += 1
+
+    # read joystick position
+    if (joy_y.value > 50000 and snake_direction != (0, -1)):
+        snake_direction = (0, 1)
+    elif (joy_y.value < 20000 and snake_direction != (0, 1)):
+        snake_direction = (0, -1)
+    elif (joy_x.value > 50000 and snake_direction != (1, 0)):
+        snake_direction = (-1, 0)
+    elif (joy_x.value < 20000 and snake_direction != (-1, 0)):
+        snake_direction = (1, 0)
+
+    if (frame_counter > snake_speed):
+        
+        # reset frame counter
+        frame_counter = 0
+
+        # delete snake tail
+        if (len(snake_body) > snake_length):
+            snake_tail = snake_body.pop(0)
+            grid[snake_tail] = 0
+
+
+        # display food
+        grid[food_pos] = food_color
+
+        # eat food
+        if (snake_pos == food_pos):
+            snake_color = food_color
+            food_pos = create_food()
+            food_color = get_random_color()
+            snake_length += 2
+
+        # render snake pos
+        grid[snake_pos] = snake_color
+        
+        # new snake pos
+        snake_pos = (snake_pos[0] + snake_direction[0], snake_pos[1] + snake_direction[1])
+
+        # gard sname pos
+        if (snake_pos[0] < 0):
+            snake_pos = (FIELD_WIDTH - 1, snake_pos[1])
+        if (snake_pos[0] >= FIELD_WIDTH):
+            snake_pos = (0, snake_pos[1])
+        if (snake_pos[1] < 0):
+            snake_pos = (snake_pos[0], FIELD_HEIGHT - 1)
+        if (snake_pos[1] >= FIELD_HEIGHT):
+            snake_pos = (snake_pos[0], 0)
+
+        snake_body.append(snake_pos)
+
+
+    time.sleep(0.01)
